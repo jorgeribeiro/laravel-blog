6be6aec8c9e0368c8f04894296bd491ae1593d2b
jorgimello <joorgemelo@gmail.com>
2018-08-11 12:31:33 -0300

    first commit

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.master')
+
+@section('content')
+	<h1>Create a post</h1>
+	<hr>
+	<form method="POST" action="/posts">
+		{{ csrf_field() }}
+		<div class="form-group">
+			<label for="exampleInputEmail1">Title</label>
+			<input type="text" class="form-control" id="title" name="title">
+		</div>
+		<div class="form-group">
+			<label for="exampleInputPassword1">Body</label>
+			<textarea type="text" class="form-control" id="body" name="body"></textarea>
+		</div>
+		
+		<button type="submit" class="btn btn-primary">Publish</button>
+	</form>
+	</br>
+@endsection
